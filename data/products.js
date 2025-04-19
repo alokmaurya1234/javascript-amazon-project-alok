1b04e6580d79a5578de263652c94bb29eefcf796
alok maurya <alokmaurya908@gmail.com>
2025-04-19 15:19:27 +0530

    use inheritance and ploymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -31,15 +31,38 @@ class Product{
 
 
   getStarsUrl(){
-    return`images/ratings/rating-${this.rating.stars * 10}.png`
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
   }
 
 
   getPrice(){
-   return`$${formatCurrency(this.priceCents)}`
+   return `$${formatCurrency(this.priceCents)}`
+  }
+
+  extraInfoHTML(){
+    return ''
   }
 }
 
+
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails)
+    this.sizeChartLink = productDetails.sizeChartLink
+  }
+
+  extraInfoHTML(){
+
+    //super.extraInfoHTML()
+    return `
+    <a href = '${this.sizeChartLink}' target = '_blank'>
+    Size chart
+    </a>
+    `
+  }
+}
 
 
 export const products = [
@@ -702,5 +725,9 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+
+    if(productDetails.type === 'clothing'){
+      return new Clothing(productDetails)
+    }
     return new Product(productDetails)
 });
